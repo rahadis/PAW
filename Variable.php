02bb7fbfc392866a895ca197cd92f098d8bda535
rahadis <neargamer700@gmail.com>
2022-03-12 16:50:11 +0700

    Menambah file variable.php

---
+++ Variable.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    $a = 40;
+    $b = 5;
+    $kali = $a*$b;
+    echo "Hasil kali $a dan $b adalah $kali ";
+    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable</title>
+</head>
+<body>
+    
+</body>
+</html>
